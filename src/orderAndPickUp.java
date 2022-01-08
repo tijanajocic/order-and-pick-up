1b65ac2c62434abe54bc73d3b0d4b7fb2399d54e
Strahinja Mitic <tttijanajocic1993@gmail.com>
2022-01-08 18:53:23 +0100

    1st step

--- src/orderAndPickUp.java
+++ src/orderAndPickUp.java
@@ -1,7 +1,21 @@
+import java.util.Scanner;
 
 public class orderAndPickUp {
 
 	public static void main(String[] args) {
+
+		Scanner s = new Scanner(System.in);
+
+		int[] price = { 50, 60, 120, 120, 140, 100, 100, 100, 80, 50, 80, 80, 80, 50, 50, 20 };
+		String[] ingredient = { "Makarone", "Spagete", "Bolognese", "Curetina", "Govedja prsuta", "Slanina", "Piletina",
+				"4 sira", "Dimljeni sir", "Parmezan", "Pavlaka", "Pesto sos", "Napolitana", "Povrce mix", "Pecurke",
+				"Kutija" };
+		String[] regularCustomer = { "0631111111", "063222222", "063333333", "064444444", "065555555", "066666666" };
+
+		System.out.println("Izvolite!");
+		System.out.println("Izaberite sastojak za pastu: ");
+		String ingredientName = s.nextLine();
+		double suma = 0;
 
 	}
 
