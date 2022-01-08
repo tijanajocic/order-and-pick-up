6728d3d42ba4409838283fc372be503377a35497
Strahinja Mitic <tttijanajocic1993@gmail.com>
2022-01-08 17:35:53 +0100

    Init project

---
+++ src/orderAndPickUp.java
@@ -0,0 +1,18 @@
+
+public class orderAndPickUp {
+
+	public static void main(String[] args) {
+
+	}
+
+	public static int findIngredient(String[] ingredients, String ingredientName) {
+
+		return 0;
+	}
+
+	public static boolean isRegularCustomer(String[] phoneNumbers, String phone) {
+
+		return false;
+	}
+
+}
